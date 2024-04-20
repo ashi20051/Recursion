47ec1433d23333fbc2030556455b3e5e80b5040c
ashi.sahu <ashi.sahu@newfold.com>
2024-04-20 17:54:13 +0530

    way 2 for reverse a number - using helper function

--- src/Level1/ReverseNumber.java
+++ src/Level1/ReverseNumber.java
@@ -3,8 +3,8 @@ package Level1;
 public class ReverseNumber {
     static int sum =0;
     public static void main(String[] args) {
-        rev1(1842);
-        System.out.println(sum);
+//        rev1(1842);
+        System.out.println(rev(1234));
     }
 
     static void rev1(int n) {
@@ -14,5 +14,17 @@ public class ReverseNumber {
         int rem = n%10;
         sum = sum*10 + rem;
         rev1(n/10);
+    }
+
+    private static int rev(int n) {
+        int digits = (int)(Math.log10(n)+1);
+        return helper(n,digits);
+    }
+
+    private static int helper(int n, int digits) {
+        if( n%10 == n) return n;
+
+        int rem = n%10;
+        return rem * (int)(Math.pow(10,digits-1)) + helper(n/10, digits-1);
     }
 }
